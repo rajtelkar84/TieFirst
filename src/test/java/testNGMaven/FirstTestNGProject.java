491e566f56b08e17d2e30647c103dd13a08a37a2
Raj Kumar Telkar <rajkumar.telkar@tieto.com>
2020-02-03 17:54:55 +0530

    2nd chnages

--- src/test/java/testNGMaven/FirstTestNGProject.java
+++ src/test/java/testNGMaven/FirstTestNGProject.java
@@ -17,7 +17,7 @@ public class FirstTestNGProject {
 	  driver.get("https://www.facebook.com/");
 	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	  System.out.println(driver.getTitle());
-	  System.out.println("hello");
+	  System.out.println("hello ji");
 	  driver.close();
   }
 }
